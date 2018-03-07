9757cc9713f3ef523580b173b7d6163aedd62c81
gossrock <gossrock@gmail.com>
2018-03-07 13:37:30 +0000

    commiting my best attempt at a 'immutable' implementation together w/ pytest test.

---
+++ immutable.py
@@ -0,0 +1,17 @@
+from typing import Any
+def immutable(mutableclass: Any): # can't quite figure out type annotation
+    class immutable:
+        def __init__(self,*args: Any, **kwds: Any) -> None:
+            super(immutable, self).__setattr__('_mutable', mutableclass(*args,**kwds))
+
+        def __getattribute__(self, s: str) -> Any:
+            try: return super(immutable,self).__getattribute__(s)
+            except AttributeError: return self._mutable.__getattribute__(s)
+
+        def __setattr__(self, name: str, value: None) -> None:
+            raise AttributeError(f"attribute '{name}' of '{self.__class__.__name__}' objects is not writable")
+
+    immutable.__name__ = mutableclass.__name__
+    immutable.__module__ = mutableclass.__module__
+
+    return immutable
---
+++ immutable_pytest.py
@@ -0,0 +1,25 @@
+from immutable import immutable
+
+from pytest import raises
+
+@immutable
+class MyTestClass:
+    def __init__(self, a: int, b: str) -> None:
+        self.a = a*2
+        self.b = b
+
+    def some_other_method(self, num: int) -> str:
+        return f'{self.a*num}--{self.b*num}'
+
+
+def test_immutable() -> None:
+    A = MyTestClass(5, 'Me')
+    assert ( A.a == 5*2 )
+    assert ( A.b == 'Me' )
+    assert ( A.some_other_method(2) == '20--MeMe' )
+
+    with raises(AttributeError):
+        A.a = 3
+
+    with raises(AttributeError):
+        A.c = 3
