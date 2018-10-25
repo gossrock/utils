c70e0d17e889757f971270797a24e6b406be9a67
gossrock <gossrock@gmail.com>
2018-10-25 13:46:46 +0000

    copy from SELF talk ... to be improved

--- command_execution/__init__.py
+++ command_execution/__init__.py
@@ -1,21 +1,12 @@
-from typing import NamedTuple, List
-
-class Command(NamedTuple):
-    command_string: str
-
-    def __str__(self) -> str:
-        return self.command_string
-
-    def to_list(self) -> List[str]:
-        return self.command_string.split()
+from typing import NamedTuple, List, Optional
+import subprocess, shlex
+from subprocess import PIPE
 
 class CommandResult(NamedTuple):
-    command:Command
+    command:str
+    return_code: int
     out:str
     error:str
-
-    
-
 
     def __str__(self) -> str:
         return_val = '=============================================\n'
@@ -28,3 +19,22 @@ class CommandResult(NamedTuple):
             return_val += self.error+'\n'
         return_val += '============================================='
         return return_val
+
+BLANK_RESULT = CommandResult("",-1, "", "")
+
+def run(command: str, stdin: Optional[str] = None, encoding: str = 'utf-8') -> CommandResult:
+    command = command.strip()
+    if '|' in command:
+        return _run_pipeline(command.split('|')) # Probobly not the best aproach
+    else:
+        process = subprocess.Popen(shlex.split(command), stdin=PIPE, stdout=PIPE, stderr=PIPE, encoding=encoding)
+        stdout, stderr = process.communicate(stdin)
+        return_code = process.wait()
+        return CommandResult(command, return_code, stdout, stderr)
+
+def _run_pipeline(commands: List[str], stdin: Optional[str] = None, encoding: str = 'utf-8') -> CommandResult:
+    command_results = BLANK_RESULT
+    for command in commands:
+        command_results = run(command, stdin=stdin, encoding=encoding)
+        stdin = command_results.out
+    return command_results
