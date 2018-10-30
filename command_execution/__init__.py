c79467da449a6ac3347aea3f45ce32451d36367c
gossrock <gossrock@gmail.com>
2018-10-30 20:59:59 +0000

    moved all the experimental code to it's proper place in the __init__ now that I see that it works. Now the syncronus run() function uses the same code as the asyncronus command execustion.

--- command_execution/__init__.py
+++ command_execution/__init__.py
@@ -1,6 +1,10 @@
 from typing import NamedTuple, List, Optional
 import subprocess, shlex, glob
-from subprocess import PIPE
+import asyncio
+from asyncio.subprocess import PIPE
+from collections.abc import AsyncGenerator
+from asyncio.subprocess import Process
+from concurrent.futures._base import TimeoutError
 
 def sub_commands(command: str) -> List[str]:
     """
@@ -56,23 +60,142 @@ class CommandResult(NamedTuple):
 BLANK_RESULT = CommandResult("",-1, "", "")
 
 def run(command: str, stdin: Optional[str] = None, encoding: str = 'utf-8') -> CommandResult:
-    command = command.strip()
-    original_command = command
-    split_commands = sub_commands(command)
+    cmd = Command(command, stdin)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(cmd.run())
+    return CommandResult(cmd.command_string, cmd.return_code, cmd.stdout, cmd.stderr)
 
-    if len(split_commands) > 1:
-        pipeline_results = _run_pipeline(split_commands)
-        return CommandResult(original_command, pipeline_results.code, pipeline_results.out, pipeline_results.error)
-    else:
-        command = expand_wildcards(command)
-        process = subprocess.Popen(shlex.split(command), stdin=PIPE, stdout=PIPE, stderr=PIPE, encoding=encoding)
-        stdout, stderr = process.communicate(stdin)
-        return_code = process.wait()
-        return CommandResult(original_command, return_code, stdout, stderr)
 
-def _run_pipeline(commands: List[str], stdin: Optional[str] = None, encoding: str = 'utf-8') -> CommandResult:
-    command_results = BLANK_RESULT
-    for command in commands:
-        command_results = run(command, stdin=stdin, encoding=encoding)
-        stdin = command_results.out
-    return command_results
+class StreamName(NamedTuple):
+    name: str
+
+STDIN = StreamName('stdin')
+STDOUT = StreamName('stdout')
+STDERR = StreamName('stderr')
+
+class OutputLine(NamedTuple):
+    data: str
+    stream: StreamName
+
+class _SingleCommand:
+    command_string: str
+    stdin: Optional[str]
+    all_output: List["OutputLine"]
+    process: Optional[Process]
+    return_code: Optional[int]
+
+    def __init__(self,  command_string: str, stdin: Optional[str]=None) -> None:
+        commands = sub_commands(command_string)
+        if len(commands) > 1:
+            raise NotImplementedError("no piplines accepted yet")
+        self.command_string = command_string
+        self.stdin = stdin
+        self.all_output = []
+        self.return_code = None
+        self.process = None
+
+    @property
+    def stdout_lines(self) -> List[str]:
+        return [output.data for output in self.all_output if output.stream == STDOUT]
+
+    @property
+    def stderr_lines(self) -> List[str]:
+        return [output.data for output in self.all_output if output.stream == STDERR]
+
+    @property
+    def stdout(self) -> str:
+        return ''.join(self.stdout_lines)
+
+    @property
+    def stderr(self) -> str:
+        return ''.join(self.stderr_lines)
+
+    async def run(self) -> None:
+        asyncio.sleep(0)
+        cmd = shlex.split(expand_wildcards(self.command_string))
+        self.process = await asyncio.create_subprocess_exec(*cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        await self._write_stdin_to_process()
+        while self.process.returncode is None:
+            await self._read_live_data(self.process.stdout, STDOUT)
+            await self._read_live_data(self.process.stderr, STDERR)
+        await self._read_leftover_data(self.process.stdout, STDOUT)
+        await self._read_leftover_data(self.process.stderr, STDERR)
+        self.return_code = self.process.returncode
+
+    async def _write_stdin_to_process(self) -> None:
+        asyncio.sleep(0)
+        if self.stdin is not None and self.process is not None and self.process.stdin is not None:
+            self.process.stdin.write(self.stdin.encode('utf-8'))
+            self.process.stdin.write_eof()
+
+    async def _read_live_data(self, stream, stream_name:StreamName) -> None:
+        asyncio.sleep(0)
+        try:
+            output_bytes = await asyncio.wait_for(stream.readline(), 0)
+            await self._store_output_line(output_bytes, stream_name)
+        except TimeoutError as e:
+            pass
+
+    async def _read_leftover_data(self, stream, stream_name:StreamName) -> None:
+        asyncio.sleep(0)
+        leftover_bytes = await stream.read(n=-1)
+        data_lines = leftover_bytes.splitlines(True)
+        for line in data_lines:
+            await self._store_output_line(line, stream_name)
+
+    async def _store_output_line(self, data: bytes, stream_name: StreamName) -> None:
+        asyncio.sleep(0)
+        if data != b"":
+            self.all_output.append(OutputLine(data.decode('utf-8'), stream_name))
+
+    async def get_live_data(self) -> AsyncGenerator:
+        index= 0
+        while self.return_code is None or len(self.all_output) > index:
+            await asyncio.sleep(0)
+            if len(self.all_output) > index:
+                yield (self.all_output[index])
+                index += 1
+
+class Command:
+    command_string: str
+    command_pipline: List[_SingleCommand]
+    stdin: Optional[str]
+
+    def __init__(self, command_string: str, stdin: Optional[str]=None) -> None:
+        self.command_string = command_string
+        self.stdin = stdin
+        commands = sub_commands(command_string)
+        self.command_pipline = []
+        for command in commands:
+            self.command_pipline.append(_SingleCommand(command))
+
+    async def run(self):
+        stdin = self.stdin
+        for command in self.command_pipline:
+            command.stdin = stdin
+            await command.run()
+            stdin = command.stdout
+
+    @property
+    def stdout(self):
+        return self.command_pipline[-1].stdout
+    @property
+    def stderr(self):
+        return self.command_pipline[-1].stderr
+
+    @property
+    def return_code(self):
+        return self.command_pipline[-1].return_code
+
+    @property
+    def stdout_lines(self) -> List[str]:
+        return self.command_pipline[-1].stdout_lines
+
+    @property
+    def stderr_lines(self) -> List[str]:
+        return self.command_pipline[-1].stderr_lines
+
+    async def get_live_data(self) -> AsyncGenerator:
+        return self.command_pipline[-1].get_live_data()
+
+class CommandManager: ...
--- command_execution/__init__pytest.py
+++ command_execution/__init__pytest.py
@@ -1,6 +1,7 @@
 import pytest
-from . import sub_commands, expand_wildcards#, run
-from .syncro_command_execution_experiment import run
+import asyncio
+from . import sub_commands, expand_wildcards, run, _SingleCommand, Command, STDOUT, STDERR
+
 import shlex
 
 @pytest.fixture(scope="module")
@@ -97,3 +98,58 @@ def test_using_fizzbuzz() -> None:
     results = run("python ./command_execution/test_command_to_run.py -F -S 15 -p 0")
     assert results.out == '3\n6\n9\n12\n15\n'
     assert results.error == '5\n10\n15\n'
+
+
+def test_basic_command_execution() -> None:
+    loop = asyncio.get_event_loop()
+
+    cmd = _SingleCommand('python ./command_execution/test_command_to_run.py -E', '123\n123')
+    loop.run_until_complete(cmd.run())
+    #print(cmd.stdout)
+    assert cmd.stdout == '123\n123'
+
+
+
+    cmd = _SingleCommand("python ./command_execution/test_command_to_run.py -F -S 15 -p 0")
+    loop.run_until_complete(cmd.run())
+    assert cmd.stdout == '3\n6\n9\n12\n15\n'
+    assert cmd.stderr == '5\n10\n15\n'
+    #print(cmd.stdout)
+    #print(cmd.stderr)
+
+    async def display(cmd) -> None:
+        #print(type(cmd.process))
+        async for d in cmd.get_live_data():
+            print(d)
+
+    cmd = _SingleCommand("python ./command_execution/test_command_to_run.py -F -S 15 -p 0.1")
+    asyncio.ensure_future(cmd.run())
+    loop.run_until_complete(display(cmd))
+
+    assert cmd.all_output[0].data == '3\n'
+    assert cmd.all_output[0].stream == STDOUT
+    assert cmd.all_output[1].data == '5\n'
+    assert cmd.all_output[1].stream == STDERR
+    assert cmd.all_output[2].data == '6\n'
+    assert cmd.all_output[2].stream == STDOUT
+    assert cmd.all_output[3].data == '9\n'
+    assert cmd.all_output[3].stream == STDOUT
+    assert cmd.all_output[4].data == '10\n'
+    assert cmd.all_output[4].stream == STDERR
+    assert cmd.all_output[5].data == '12\n'
+    assert cmd.all_output[5].stream == STDOUT
+    assert cmd.all_output[6].data == '15\n'
+    assert cmd.all_output[6].stream == STDOUT or cmd.all_output[6].stream == STDERR
+    assert cmd.all_output[7].data == '15\n'
+    assert cmd.all_output[7].stream == STDOUT or cmd.all_output[7].stream == STDERR
+
+def test_pipeline() -> None:
+    loop = asyncio.get_event_loop()
+
+    cmd = Command("echo 'test' | python ./command_execution/test_command_to_run.py -E")
+    loop.run_until_complete(cmd.run())
+    assert cmd.stdout == 'test\n'
+
+    cmd = Command("ls command_execution/__init__* | grep 'pytest'")
+    loop.run_until_complete(cmd.run())
+    #print(cmd.stdout.encode('utf-8'))
